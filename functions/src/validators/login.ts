896ec123fa9551c217eca137de4b8d53ef7ade00
Habideen Ibrahim <habideenibrahim@gmail.com>
2025-10-08 05:34:51 +0100

    login

--- functions/src/validators/login.ts
+++ functions/src/validators/login.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
+  // optinal device field to determine if cookies should be set
+  device: z.string().optional(),
   email: z
     .string({ required_error: "Email is required" })
     .email("Invalid email"),
