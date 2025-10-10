b78feb26a830256a624a75a2acd1763148eb4767
Habideen Ibrahim <habideenibrahim@gmail.com>
2025-10-10 09:58:26 +0100

    upload data

--- functions/src/routes/v1/farmerRoutes.ts
+++ functions/src/routes/v1/farmerRoutes.ts
@@ -4,13 +4,6 @@ import { farmersUpload } from "../../controllers/v1/farmer/upload";
 
 const farmerRouter = express.Router();
 
-// Create a sub-router for protected routes
-const protectedRoutes = express.Router();
-
-// Field Officer–only routes
-protectedRoutes.post("/upload", farmersUpload);
-
-// Apply AuthGuard only to this subset
-farmerRouter.use("/", AuthGuard(["Field Officer"]), protectedRoutes);
+farmerRouter.post("/upload", AuthGuard(["Field Officer"]), farmersUpload);
 
 export default farmerRouter;
