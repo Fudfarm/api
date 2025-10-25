313aaf73a52a1f8651283986056ab93e7a0b2fb1
Habideen Ibrahim <habideenibrahim@gmail.com>
2025-10-25 19:25:28 +0100

    fix

--- functions/src/function/variables.ts
+++ functions/src/function/variables.ts
@@ -9,6 +9,6 @@ export type IMaritalStatus = (typeof maritalStatuses)[number];
 
 export const SERVER = {
   ASSET_BUCKET_URL: "fudfarmer-c78cb.firebasestorage.app",
-  FARMER_IMAGE_PATH: "images/farmer",
-  FARMER_CONSENT_PATH: "video/consent/farmer",
+  FARMER_IMAGE_PATH: "images/users",
+  FARMER_CONSENT_PATH: "videos/consent",
 };
