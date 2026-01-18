ec3b4c59bd129c367f1540c4313ad6f58caf7211
Habideen Ibrahim <habideenibrahim@gmail.com>
2026-01-18 19:23:06 +0100

    update shop item

--- functions/src/validators/farmer/shop_items.ts
+++ functions/src/validators/farmer/shop_items.ts
@@ -5,7 +5,12 @@ export const shopItemSchema = z.object({
   recordID: z.string().uuid().optional(),
 
   item: z.string().trim().min(1, "Item is required"),
-  quantity: z.number().int().min(0, "Quantity must be a positive integer").optional(),
+  quantity: z
+    .number()
+    .int()
+    .min(0, "Quantity must be a positive integer")
+    .optional(),
+  unitId: z.string().uuid("Invalid uuid supplied"),
   category: z.enum(["Crop", "Animal", "Equipment", "Other"]),
   verified: z.boolean().optional(),
 });
